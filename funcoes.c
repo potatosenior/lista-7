2ac2a3e7b0fbf89aed15c828efa6ae0815ac2e8e
Joao - Notebook <potatosenior6@hotmail.com>
2019-11-11 10:54:30 -0200

    assasassa

---
+++ funcoes.c
@@ -0,0 +1,31 @@
+//
+// Created by Marco Antonio on 11/11/2019.
+//
+
+int exercicio1(int num)
+{
+    return 2*num;
+}
+
+void exercicio2(char data[11])
+{
+    int dia = 0, mes = 0, ano = 0;
+    char mess[20][12] = {"janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outrobro", "novembro", "dezembro"};
+
+    dia = ( (data[0] - 48) * 10) + (data[1] - 48);
+    mes = (data[3] - 48) * 10 + data[4] - 48;
+    ano = (data[6] - 48) * 1000 + (data[7] - 48) * 100 + (data[8] - 48) * 10 + data[9] - 48;
+
+    printf("\n%d de %s de %d", dia, mess[mes-1], ano);
+}
+
+int exercicio3(int num)
+{
+    if (num > 0)
+        return 1;
+    else
+        if (num < 0)
+            return -1;
+        else
+            return 0;
+}
---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include "funcoes.c"
+
+int main() {
+    int exercicio = 1;
+
+    while (exercicio != 0)
+    {
+        printf("\n\nDigite o exercicio: ");
+        scanf("%d", &exercicio);
+        setbuf(stdin, NULL);
+
+        switch (exercicio)
+        {
+            case 1:
+            {
+                int numero, resultado;
+
+                printf("Digite um numero: ");
+                scanf("%d", &numero);
+
+                resultado = exercicio1(numero);
+                printf("O dobro de %d eh: %d", numero, resultado);
+                break;
+            }
+
+            case 2:
+            {
+                char data[11];
+
+                printf("Digite a data: ");
+                fgets(data, 11, stdin);
+                //printf("data: %s, %d", data, strlen(data));
+
+                if (data[2] != '/' || data[5] != '/' || strlen(data) != 10){
+                    printf("\nFormato invalido!");
+                }else
+                    exercicio2(data);
+
+                break;
+            }
+
+            case 3:
+            {
+                int numero, resultado;
+
+                printf("Digite um numero: ");
+                scanf("%d", &numero);
+
+                resultado = exercicio3(numero);
+                printf("Resultado: %d", resultado);
+
+                break;
+            }
+
+            case 4:
+            {
+
+            }
+
+            default:
+            {
+                if (exercicio == 0)
+                    printf("\nSaindo do progama...");
+                else
+                    printf("\nExercicio invalido!");
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
